cdde3ae68a589fbec775d986a17decd488588e5d
suyundykovv <margulansuyundykov@gmail.com>
2024-10-02 23:29:16 +0500

    add more features

--- models/Economy.java
+++ models/Economy.java
@@ -1,13 +1,74 @@
 package models;
 
 public class Economy {
-    private int economySkill;
+    private int money; // Current amount of money
+    private int economicPoints; // Economic points for income calculation
+    private int maintenanceCostPerSoldier; // Maintenance cost for soldiers
+    private Military military; // Reference to Military class
+    private static final int DUCAT_VALUE = 10; // Value of each economic point in ducats
 
-    public Economy(int economySkill) {
-        this.economySkill = economySkill;
+    // Constructor
+    public Economy(int initialMoney, int economicPoints) {
+        this.money = initialMoney;
+
+        this.maintenanceCostPerSoldier = 1; // Set to 1 ducat per soldier for simplicity
+        this.economicPoints = 3; // Base economic points
     }
 
-    public int getEconomySkill() {
-        return economySkill;
+    // Method to calculate income based on economic points
+    public void calculateIncome() {
+        int income = economicPoints * DUCAT_VALUE; // Income calculation
+        money += income; // Add income to total money
+    }
+
+    // Method to update economic points
+    public void updateEconomicPoints(int change) {
+        economicPoints += change;
+    }
+
+    // Method to add money
+    public void addMoney(int amount) {
+        money += amount;
+    }
+
+    // Method to subtract money (e.g., for upgrades, spending)
+    public boolean subtractMoney(int amount) {
+        if (money >= amount) {
+            money -= amount;
+            return true; // Successful transaction
+        } else {
+            System.out.println("Not enough money!");
+            return false; // Insufficient funds
+        }
+    }
+
+    // Method to calculate and deduct maintenance costs for soldiers
+    public void deductMaintenanceCosts() {
+        int totalMaintenanceCost = military.getSoldiers() * maintenanceCostPerSoldier;
+        if (subtractMoney(totalMaintenanceCost)) {
+            System.out.println("Maintenance costs deducted: " + totalMaintenanceCost + " ducats");
+        }
+    }
+
+    // Method to recruit soldiers
+    public void recruitSoldiers(int numberOfRecruits) {
+        int cost = numberOfRecruits; // 1 ducat per recruit
+        if (subtractMoney(cost)) {
+            military.recruitSoldiers(numberOfRecruits); // Recruit soldiers using Military class
+            System.out.println("Recruited " + numberOfRecruits + " soldiers.");
+        }
+    }
+
+    // Getters
+    public int getMoney() {
+        return money;
+    }
+
+    public int getEconomicPoints() {
+        return economicPoints;
+    }
+
+    public Military getMilitary() {
+        return military;
     }
 }
--- models/Military.java
+++ models/Military.java
@@ -1,13 +1,35 @@
 package models;
 
 public class Military {
-    private int militarySkill;
+    private int soldiers; // Current number of soldiers
+    private int militaryPoints; // Military points for calculating recruits
+    private static final int RECRUITS_PER_MILITARY_POINT = 30; // Recruits per military point
 
-    public Military(int militarySkill) {
-        this.militarySkill = militarySkill;
+    // Constructor
+    public Military(int initialSoldiers, int initialMilitaryPoints) {
+        this.soldiers = initialSoldiers;
+        this.militaryPoints = initialMilitaryPoints;
     }
 
-    public int getMilitarySkill() {
-        return militarySkill;
+    // Method to recruit soldiers based on military points
+    public void recruitSoldiers(int numberOfRecruits) {
+        soldiers += numberOfRecruits; // Increase soldier count
+    }
+
+    // Method to calculate total recruits available
+    public int getAvailableRecruits() {
+        return militaryPoints * RECRUITS_PER_MILITARY_POINT; // Total recruits available based on military points
+    }
+
+    // Getters
+    public int getSoldiers() {
+        return soldiers;
+    }
+    public int getMilitaryPointsPoints() {
+        return militaryPoints;
+    }
+    public void setMilitaryPoints(int militaryPoints) {
+        this.militaryPoints = militaryPoints;
     }
 }
+
